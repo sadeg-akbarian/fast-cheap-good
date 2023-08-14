e00174b10c91e0ff26ae04bffc6a8b5a409b5348
sadeg-akbarian <sadeg.akbarian@gmail.com>
2023-08-14 13:45:20 +0200

    feat: project completed

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ fastCheckbox.addEventListener("input", function (event) {
   if (fastCheckbox.hasAttribute("checked") === false) {
     fastCheckbox.setAttribute("checked", "");
     checkedChechboxes.push(event.target);
-    console.log(checkedChechboxes);
-    document.body.style.backgroundColor = "blue";
     if (
       cheapCheckbox.hasAttribute("checked") &&
       goodCheckbox.hasAttribute("checked")
@@ -21,63 +19,52 @@ fastCheckbox.addEventListener("input", function (event) {
       if (index > -1) {
         checkedChechboxes.splice(index, 1);
       }
-      console.log(lastCheckedCheckbox);
     }
   } else {
     fastCheckbox.removeAttribute("checked");
-    document.body.style.backgroundColor = "blue";
   }
-  console.log(event.target);
 });
 
 cheapCheckbox.addEventListener("input", function (event) {
   if (cheapCheckbox.hasAttribute("checked") === false) {
     cheapCheckbox.setAttribute("checked", "");
     checkedChechboxes.push(event.target);
-    console.log(checkedChechboxes);
-    document.body.style.backgroundColor = "red";
     if (
       fastCheckbox.hasAttribute("checked") &&
       goodCheckbox.hasAttribute("checked")
     ) {
       const arrayLength = checkedChechboxes.length;
       const lastCheckedCheckbox = checkedChechboxes[arrayLength - 2];
+      lastCheckedCheckbox.removeAttribute("checked");
       lastCheckedCheckbox.checked = false;
       const index = checkedChechboxes.indexOf(lastCheckedCheckbox);
       if (index > -1) {
         checkedChechboxes.splice(index, 1);
       }
-      console.log(lastCheckedCheckbox);
     }
   } else if (cheapCheckbox.hasAttribute("checked") === true) {
     cheapCheckbox.removeAttribute("checked");
-    document.body.style.backgroundColor = "blue";
   }
-  console.log(event.target);
 });
 
 goodCheckbox.addEventListener("input", function (event) {
   if (goodCheckbox.hasAttribute("checked") === false) {
     goodCheckbox.setAttribute("checked", "");
     checkedChechboxes.push(event.target);
-    console.log(checkedChechboxes);
-    document.body.style.backgroundColor = "blue";
     if (
       cheapCheckbox.hasAttribute("checked") &&
       fastCheckbox.hasAttribute("checked")
     ) {
       const arrayLength = checkedChechboxes.length;
       const lastCheckedCheckbox = checkedChechboxes[arrayLength - 2];
+      lastCheckedCheckbox.removeAttribute("checked");
       lastCheckedCheckbox.checked = false;
       const index = checkedChechboxes.indexOf(lastCheckedCheckbox);
       if (index > -1) {
         checkedChechboxes.splice(index, 1);
       }
-      console.log(lastCheckedCheckbox);
     }
   } else {
     goodCheckbox.removeAttribute("checked");
-    document.body.style.backgroundColor = "blue";
   }
-  console.log(event.target);
 });
